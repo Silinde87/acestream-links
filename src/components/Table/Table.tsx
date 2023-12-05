e2d9c393c2828ae9528e38f8cc6b05859daa437d
Silinde87 <paurodriguezmolina@gmail.com>
2023-12-05 23:30:42 +0100

    feat: Add more columns to table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -7,8 +7,13 @@ const Table: React.FC<TTable> = ({ links }) => {
 			<tr key={`table-link-${index}`}>
 				<td>{link.name}</td>
 				<td>
-					<a href={link.hash}>{link.hash}</a>
+					<a href={link.hash}>{'...' + link.hash.slice(-8)}</a>
 				</td>
+				<td>{link.channel || '-'}</td>
+				<td>{link.competition || '-'}</td>
+				<td>{link.language}</td>
+				<td>{link.sport}</td>
+				<td>{link.video_quality || '-'}</td>
 			</tr>
 		));
 	};
@@ -19,6 +24,11 @@ const Table: React.FC<TTable> = ({ links }) => {
 				<tr>
 					<th>Name</th>
 					<th>Hash</th>
+					<th>Channel</th>
+					<th>Competition</th>
+					<th>Language</th>
+					<th>Sport</th>
+					<th>Quality</th>
 				</tr>
 			</thead>
 			<tbody>{renderRow()}</tbody>
